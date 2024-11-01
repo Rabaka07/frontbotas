530702e7d5c1ea5bec0fc18117637542ae1eb840
Rabaka07 <rabaquimruan07@gmail.com>
2024-11-01 14:02:13 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+  )
+}
+
+export default App
